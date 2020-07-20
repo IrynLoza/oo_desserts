43f39a9dd1f41c371a3c4a827155529f91d11d3e
Iryna Loza <irynloza@gmail.com>
2020-07-20 11:58:57 -0700

    Create __init__ and add_stock methods

--- desserts.py
+++ desserts.py
@@ -3,16 +3,24 @@
 
 class Cupcake:
     """A cupcake."""
+    cache = {}
 
-    def __init__(self, name, qty):
-        self.name = name
-        self.qty = qty
-
+    def __init__(self, name, flavor, price, qty=0):
+        self.name = str(name)
+        self.flavor = str(flavor)
+        self.price = float(price)
+        self.qty = int(qty)
+        self.cache[name] = self
+        
 
     def __repr__(self):
         """Human-readable printout for debugging."""
 
         return f'<Cupcake name="{self.name}" qty={self.qty}>'
+
+
+    def add_stock(self, amount):
+        self.qty = self.qty + amount
 
 
 if __name__ == '__main__':
